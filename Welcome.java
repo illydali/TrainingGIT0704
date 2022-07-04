85ed68c035b6e54879cd87326454af794d780d1c
DEV <dev@dev.com>
2022-07-05 00:19:17 +0530

    commit..

---
+++ Welcome.java
@@ -0,0 +1,8 @@
+class Welcome {
+	public static void main(String args[]) {
+	String message = "Welcome";
+	// comment-line entries (readability of the program)
+	System.out.println(message);
+	}
+}
+
