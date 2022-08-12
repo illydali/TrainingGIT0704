0e25a5d9972795266b50d5ce3125263b47c72a3e
dev <dev@dev.com>
2022-08-12 09:03:07 +0530

    commit..

--- 6 ANGULAR/EFictiShopAngularSPA/src/app/app-routing.module.ts
+++ 6 ANGULAR/EFictiShopAngularSPA/src/app/app-routing.module.ts
@@ -4,20 +4,25 @@ import { AddEmpSbComponent } from "./add-emp-sb/add-emp-sb.component";
 import { AddEmpComponent } from "./add-emp/add-emp.component";
 import { ListEmpSbComponent } from "./list-emp-sb/list-emp-sb.component";
 import { ListEmpComponent } from "./list-emp/list-emp.component";
+import { LoginComponent } from "./login/login.component";
+import { AuthGuardService } from "./service/auth-guard.service";
 import { UpdateEmpSbComponent } from "./update-emp-sb/update-emp-sb.component";
 import { UpdateEmpComponent } from "./update-emp/update-emp.component";
 
 
 const routes: Routes = [
     // localhost:4200 (localhost:4200/employees)
-    {path : '', redirectTo: 'employees-sb', pathMatch: 'full'},
+    // {path : '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '', component:LoginComponent},
+    {path: 'login', component:LoginComponent},
     // employees = load ListEmpComponent
-    {path: 'employees', component:ListEmpComponent},
-    {path: 'employees-sb', component:ListEmpSbComponent},
-    {path: 'add', component:AddEmpComponent},
-    {path: 'add-sb', component:AddEmpSbComponent},
-    {path: 'update/:id', component:UpdateEmpComponent},
-    {path: 'update-sb/:id', component:UpdateEmpSbComponent}
+    {path: 'employees', component:ListEmpComponent, canActivate:[AuthGuardService]},
+    {path: 'employees-sb', component:ListEmpSbComponent, canActivate:[AuthGuardService]},
+    {path: 'add', component:AddEmpComponent, canActivate:[AuthGuardService]},
+    {path: 'add-sb', component:AddEmpSbComponent, canActivate:[AuthGuardService]},
+    {path: 'update/:id', component:UpdateEmpComponent, canActivate:[AuthGuardService]},
+    {path: 'update-sb/:id', component:UpdateEmpSbComponent, canActivate:[AuthGuardService]},
+    {path: '**', component:LoginComponent},
 ]
 
 @NgModule({
